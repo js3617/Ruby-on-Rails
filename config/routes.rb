8b31d2e505aaf134719e0b1d83f743d705554a4e
js3617 <aa74774574@gmail.com>
2024-10-04 05:39:01 +0000

    Add - articles, comments 기능 구현

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+	resources :articles do
+		resources :comments, only: [:create]
+	end
+	
+  resources :class_statuses
+  resources :class_lists
+  devise_for :users
   resources :tests
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 	
-	root 'tests#index'
+	root 'articles#index'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_09_27_050451) do
+ActiveRecord::Schema.define(version: 2024_10_04_042425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "comment"
+    t.bigint "article_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["article_id"], name: "index_comments_on_article_id"
+  end
 
   create_table "tests", force: :cascade do |t|
     t.string "name"
@@ -22,4 +37,17 @@ ActiveRecord::Schema.define(version: 2024_09_27_050451) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  add_foreign_key "comments", "articles"
 end
